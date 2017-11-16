88af509cf2df1d6d6911f5bd3626aad2962168f4
juergen.kussmann <juergen.kussmann@aoe.com>
2017-11-16 15:51:49 +0100

    [CLEANUP] remove compatibility to TYPO3 6.2

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,11 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-if (class_exists(\TYPO3\CMS\Core\Imaging\IconRegistry::class)) {
+call_user_func(function () {
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
     $iconRegistry->registerIcon(
         'tx-google-tag-manager',
         \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
         ['source' => 'EXT:google_tag_manager/Resources/Public/Images/Backend/GoogleTagManager.gif']
     );
-}
+});
